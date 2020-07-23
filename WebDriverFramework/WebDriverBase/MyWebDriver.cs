4ade10ad99b3738ec3915d13445202ebb82efb49
Sergiu Alexandru Antal <sergiu.antal@endava.com>
2020-07-23 13:51:03 +0300

    Add project files.

---
+++ WebDriverFramework/WebDriverBase/MyWebDriver.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebDriverFramework.WebDriverBase
+{
+    public class MyWebDriver
+    {
+        private ChromeOptions _chromeOptions;
+        private ChromeDriver _chromeDriver;
+
+        public ChromeOptions ChromeOptions { get => _chromeOptions; set => _chromeOptions = value; }
+
+        public MyWebDriver()
+        {
+            _chromeDriver = new ChromeDriver(_chromeOptions);
+            _chromeDriver.Manage().Window.Maximize();
+            _chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+        }
+
+        public void SetChromeDriverOptions(ChromeOptions chromeOptions)
+        {
+            _chromeOptions = chromeOptions;
+        }
+
+
+
+    }
+}
---
+++ WebDriverFramework/WebDriverBase/MyWebDriverFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebDriverFramework.WebDriverBase
+{
+    public class MyWebDriverFactory
+    {
+        private MyWebDriver _myWebDriver;
+        public void InitializeBrowser()
+        {
+            _myWebDriver = new MyWebDriver();
+        }
+
+    }
+}
